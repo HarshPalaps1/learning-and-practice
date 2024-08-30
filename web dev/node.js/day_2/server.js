c06a3058e54bc156b57738b15430b85a91fea58a
Harsh pal <86770957+HarshPalaps1@users.noreply.github.com>
2024-08-30 20:00:34 +0530

    server/request

--- web dev/node.js/day_2/server.js
+++ web dev/node.js/day_2/server.js
@@ -9,11 +9,27 @@ const HOSTNAME="localhost"
 
 // function for creating http server
 const server=http.createServer((req,res)=>{
-
+    if(req.url="/"){
+        res.statusCode=200;
+        res.setHeader("Content-Type","text/plain");
+        res.end("Node Sever created by Chirag Goel!");
+    }
+    else if(req.url="/about"){
+        res.statusCode=200;
+        res.setHeader("Content-Type","text/plain");
+        res.end("about page!");
+    }
+    else if(req.url="/contact"){
+        res.statusCode=200;
+        res.setHeader("Content-Type","text/plain");
+        res.end("Email:- pramodishupal@gmail.com");
+    }else {
+        res.statusCode=500;
+        res.setHeader("Content-Type","text/plain");
+        res.end("PAGE DO NOT EXIST");
+    }
     
-    res.statusCode=200;
-    res.setHeader("Content-Type","text/plain");
-    res.end("Node Sever created by Chirag Goel!");
+   
 });
 
 // function to fix the port ,output after    calling server
